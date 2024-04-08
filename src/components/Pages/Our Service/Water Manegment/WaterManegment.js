dde1697e916be9d6a67e916c370524144fed450a
JaseWils <jasewilson5566@gmail.com>
2024-04-08 12:25:47 +0530

    hvkhv

--- src/components/Pages/Our Service/Water Manegment/WaterManegment.js
+++ src/components/Pages/Our Service/Water Manegment/WaterManegment.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function WaterManegment() {
   return (
     <div>
-      <h1> Water Manegment bishak</h1>
+      <h1> Water Manegment</h1>
     </div>
   )
 }
