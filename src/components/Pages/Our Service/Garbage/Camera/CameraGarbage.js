bc0463d6d65b6de1748b9b295e385706a132c01f
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-04-13 01:53:03 +0530

    updated garbagemanagement

--- src/components/Pages/Our Service/Garbage/Camera/CameraGarbage.js
+++ src/components/Pages/Our Service/Garbage/Camera/CameraGarbage.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useContext, useRef } from "react";
+import CameraContext from "../context/CameraContext";
 
 const CameraGarbage = (props) => {
-   // class1-->Stand for position the camera button using className
+  // class1-->Stand for position the camera button using className
   // modal-title--> take titlte
   // related-text-->gives a content brief description about the topic
   // photo_height,photo_width--> set the  acccepted imaGE height width
-  // img_src--> is the default bgimage1 place on the camera modal
+  // img_src--> is the default garbageimg place on the camera modal
   // modal-type---> stand for  target modal
   // specific_img--->this is used to select a particular div for a particular modal(by not changing all div){you can say as target}(imp to add)
   const {
@@ -19,19 +20,19 @@ const CameraGarbage = (props) => {
     specific_img,
   } = props;
 
-  const { bgimage1, setBgimage1 } = useContext(CameraContext);
+  const { garbageimg,setGarbageimg } = useContext(CameraContext);
 
   const refclose = useRef(null);
   const handleUpload = () => {
     refclose.current.click();
     document.getElementById(specific_img).value = "";
 
-    // setBgimage1({...bgimage1,[event.target.id]:[event.target.value]})
-    // setBgimage1('')
+    // setGarbageimg({...garbageimg,[event.target.id]:[event.target.value]})
+    // setGarbageimg('')
   };
 
   const changing = (event) => {
-    setBgimage1(event.target.files[0]);
+    setGarbageimg(event.target.files[0]);
   };
 
   var image_body = {
@@ -41,23 +42,15 @@ const CameraGarbage = (props) => {
   };
   return (
     <>
-      {/* camera bgimage1 */}
+      {/* camera garbageimg */}
       <span
         className={`profile-file-upload p-1 mx-2 perfect ${class1} my-0`}
-        data-bs-toggle="modal"
-        data-bs-target={`#${modal_type}`}
+        
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          className="bi bi-camera-fill"
-          viewBox="0 0 16 16"
-        >
-          <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
-          <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0" />
-        </svg>
+        <button class="button mx-2 py-2 border-0" type="button" data-bs-toggle="modal"
+        data-bs-target={`#${modal_type}`}>
+          Create Post For Garbage Problem
+        </button>
       </span>
       {/* camera open modal */}
 
@@ -88,18 +81,18 @@ const CameraGarbage = (props) => {
                 <h6>
                   <i style={{ color: "brown" }}>{related_text}</i>
                 </h6>
-                {bgimage1 ? (
+                {garbageimg ? (
                   <img
                     className="container mb-3 d-flex justify-content-center"
                     style={image_body}
-                    src={URL.createObjectURL(bgimage1)}
+                    src={URL.createObjectURL(garbageimg)}
                     id="view1"
-                    alt="bgimage1"
+                    alt="garbageimg"
                   />
                 ) : (
                   <img
                     className="container mb-3 d-flex justify-content-center"
-                    alt="bgimage1"
+                    alt="garbageimg"
                     style={image_body}
                     src={img_src}
                     id="view0"
@@ -151,6 +144,4 @@ CameraGarbage.defaultProps = {
   // specific_img:'modalimg'
 };
 
-
-
-export default CameraGarbage
+export default CameraGarbage;
--- src/components/Pages/Our Service/Garbage/GarbageManagement.js
+++ src/components/Pages/Our Service/Garbage/GarbageManagement.js
@@ -5,20 +5,22 @@ import { useContext } from "react";
 import CameraContext from "./context/CameraContext";
 import CameraProfile from "./Camera/CameraProfile";
 import CameraBg from "./Camera/CameraBg";
+import GarbagemainBody from "./GarbagemainBody";
+
 
 const GarbageManagement = () => {
-  const {image,bgimage1}=useContext(CameraContext)
+  const { image, bgimage1 } = useContext(CameraContext)
   return (
     <div className="garbage-body">
-      <div className="">
+      <div className="topHeight">
         <GarbageHeader />
-        <span style={{ position: "relative" }}>
+        <span style={{ position: "relative" }} className="">
           {/* <img
             src="https://source.unsplash.com/random/1536x500?/nature"
             className=" img-fluid"
             alt="background image"
           /> */}
-          {bgimage1?(<img className="img-fluid profile-bg-fix"  src={URL.createObjectURL(bgimage1)} id="view1" />):(<img
+          {bgimage1 ? (<img className="img-fluid profile-bg-fix" src={URL.createObjectURL(bgimage1)} id="view1" />) : (<img
             src="https://source.unsplash.com/random/1536x500?/nature"
             className=" img-fluid profile-bg-fix"
             alt="background image"
@@ -29,12 +31,12 @@ const GarbageManagement = () => {
               className=" img-fluid img-style mx-2"
               alt="profile"
             /> */}
-            {image?(<img className="img-fluid img-style mx-2 profile_condition"  src={URL.createObjectURL(image)} id="view1" />):(<img
+            {image ? (<img className="img-fluid img-style mx-2 profile_condition" src={URL.createObjectURL(image)} id="view1" />) : (<img
               src="profile.png"
               className=" img-fluid img-style mx-2 profile_condition"
-              alt="profile"/>)}
+              alt="profile" />)}
             <CameraProfile class1="extra-pos" photo_height="140px" photo_width="150px" img_src="profile.png" modal_type="profile_photo" modal_title="Upload Profile Photo" specific_img="modal1" />
-  <span className="fs-1 mx-3" style={{ ffontWeight: "800" }}>
+            <span className="fs-1 mx-3" style={{ ffontWeight: "800" }}>
               Soumadip Santra
             </span>
           </div>
@@ -49,12 +51,18 @@ const GarbageManagement = () => {
               />
               Privacy
             </button>
-            <CameraBg class1="" photo_height="500px" photo_width="1536px" img_src="https://source.unsplash.com/random/1536x500?/nature" modal_type="profile_bg" modal_title="Upload Profile-BackGround Photo" specific_img="modal2"/>
-              
-            
+            <CameraBg class1="" photo_height="500px" photo_width="1536px" img_src="https://source.unsplash.com/random/1536x500?/nature" modal_type="profile_bg" modal_title="Upload Profile-BackGround Photo" specific_img="modal2" />
+
+
           </div>
+          <p className="float-start mt-3"><button type="button" class="btn btn-primary px-5 py-2" style={{position:'relative',left:'212px'}}>Message User</button></p>
         </span>
+        <hr className="mt-5" style={{position:'relative',top:'27px',    right:'79px'}}/>
       </div>
+        <div className="container my-5 ">
+          <GarbagemainBody />
+        </div>
+        
     </div>
   );
 };
