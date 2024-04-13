aa0a82c429e6e6d1b03d3c1385e4866486e643e7
Shreya Das <dshreya943@gmail.com>
2024-04-13 20:28:56 +0530

    shreya

--- src/components/Pages/Home/DashboardBody.js
+++ src/components/Pages/Home/DashboardBody.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from 'react';
+
+import "./DashboardBody.css";
+
 
 const DashboardBody = () => {
-  return (
-    <div className="container">
-    <div className='my-5'>
-      hello boy
-    </div></div>
-  )
-}
 
-export default DashboardBody
+  return (
+    <div className="background-container" src="pollution.jpg">
+      <div className='container1'>
+        <div className="card" style={{width:'18rem'}}>
+          <div className="card-body">
+            <h5 className="card-title">WATER MANAGEMENT</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">more info</h6>
+            
+          </div>
+        </div>
+      </div>
+
+      <div className='container2'>
+        <div className="card" style={{width:'18rem'}}>
+          <div className="card-body">
+            <h5 className="card-title">AIR MANAGEMENT</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">more info</h6>
+            
+          </div>
+        </div>
+      </div>
+
+      <div className='container3'>
+        <div className="card" style={{width:'20rem'}}>
+          <div className="card-body">
+            <h5 className="card-title">GARBAGE MANAGEMENT</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">more info</h6>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardBody;
